425676ce9ded5d1911ec6238d191e9f849bb0e87
ellwarco <s.shmyrev@laposte.net>
2018-05-13 09:23:39 +0200

    toolbox

--- src/pages/scoreboard.jsx
+++ src/pages/scoreboard.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import HeadlineWithFocus from "../components/HeadlineWithFocus";
-import Fetchdata from "../components/Fetchdata";
 import Linear from "../components/Linear";
 import Helmet from "react-helmet";
 
@@ -8,7 +7,7 @@ export default () => (
   <div className="c-content-box">
     <Helmet title="Sunday5K ScoreBoard" />
     <HeadlineWithFocus>
-      <Fetchdata />
+     
     <Linear />
     </HeadlineWithFocus>
     <p>Coming soon...</p>
--- src/pages/toolbox.jsx
+++ src/pages/toolbox.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import HeadlineWithFocus from '../components/HeadlineWithFocus';
+import Fetchdata from "../components/Fetchdata";
 import Helmet from 'react-helmet';
-
-import ToolBoxListContainer from '../containers/ToolBoxListContainer';
 
 export default () => (
   <div className="c-content-box">
     <Helmet title="That's my running toolbox" />  
 
     <HeadlineWithFocus><span>These are pieces of </span>my RunBox</HeadlineWithFocus>
-    <ToolBoxListContainer />
+    <Fetchdata />
    
   </div>
 );
